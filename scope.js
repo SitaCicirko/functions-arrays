60f0d2e12ea80da5000933b591006aab944e6fa8
Sita Cicirko <ns.cicirko@gmail.com>
2024-12-10 13:37:23 +0000

    1 commit

---
+++ scope.js
@@ -0,0 +1,27 @@
+console.log("Hello world!");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+console.log(blogPosts);
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog post";
+console.log(blogPosts[3]); //
+
+const randomList = ["important reminder", 42, ["another array"], true];
+console.log(randomList);
+
+const favouriteFoods = ["Pizza", "Sushi", "Ice cream"];
+console.log(favouriteFoods);
+
+const favouriteColours = ["Black", "Blue", "Green"];
+console.log(favouriteColours);
+
+const favouriteNumbers = [15, 25, 22];
+console.log(favouriteNumbers);
